f472e8d361c752a5186f76ce582d422683f677c8
UH <554c46@gmail.com>
2020-12-14 10:28:12 +0100

    flake8 fixes

--- augtxt/wordsubs.py
+++ augtxt/wordsubs.py
@@ -16,7 +16,7 @@ def lookup_synonyms_fasttext(words: List[str], lang: str,
                              max_neighbors: Optional[int] = 100,
                              min_vector_score: Optional[float] = 0.65,
                              max_shingle_score: Optional[float] = 0.35,
-                             kmax : Optional[int] = 8,
+                             kmax: Optional[int] = 8,
                              n_max_wildcards: Optional[int] = None,
                              path_buffer: Optional[str] = PATH_BUFFER,
                              path_fasttext: Optional[str] = PATH_FASTTEXT
@@ -24,13 +24,17 @@ def lookup_synonyms_fasttext(words: List[str], lang: str,
     """Lookup synonyms for a given list of words
     """
     # create buffer folder if not exist
-    os.makedirs(buffer_path, exist_ok=True)
+    os.makedirs(path_buffer, exist_ok=True)
 
     # load buffer file
-    with open(os.path.join(path_buffer, f"{lang}.json"), 'r') as fp:
-        buffer = json.load(fp)
+    try:
+        with open(os.path.join(path_buffer, f"{lang}.json"), 'r') as fp:
+            buffer = json.load(fp)
+    except Exception:
+        buffer = {}
 
     # load fasttext model
+    print("[INFO] It takes several minutes to load a FastText model")
     model = fasttext.load_model(
         os.path.join(path_fasttext, f"wiki.{lang}.bin"))
 
@@ -49,15 +53,17 @@ def lookup_synonyms_fasttext(words: List[str], lang: str,
             # extract synonyms from fasttext
             results = model.get_nearest_neighbors(word, max_neighbors)
             # compute jaccard similarity based on k-shingles
-            results = [(ks.jaccard_strings(word, syn, k=kmax), vectorscore, syn)
-                       for vectorscore, syn in results]
+            results = [
+                (ks.jaccard_strings(word, syn, k=kmax), vectorscore, syn)
+                for vectorscore, syn in results]
             # filter results
             results = [syn for shinglescore, vectorscore, syn in results
-                       if shinglescore <= max_shingle_score 
-                       and vectorscore >= min_vector_score]
+                       if (shinglescore <= max_shingle_score) and (
+                           vectorscore >= min_vector_score)]
             # update buffer and save results
-            buffer[word] = results
-            synonyms[word] = results
+            if results:
+                buffer[word] = results
+                synonyms[word] = results
 
     # save updated buffer file
     with open(os.path.join(path_buffer, f"{lang}.json"), 'w') as fp:
@@ -70,4 +76,3 @@ def lookup_synonyms_fasttext(words: List[str], lang: str,
 
     # done
     return synonyms
-
