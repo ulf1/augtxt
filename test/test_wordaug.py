e45f7aa53d97d13a6a3a6092e76651086a490b9e
UH <554c46@gmail.com>
2021-12-26 18:36:08 +0100

    unit tests added

---
+++ test/test_wordaug.py
@@ -0,0 +1,44 @@
+from augtxt.augmenters import wordaug
+import augtxt.keyboard_layouts as kbl
+import numpy as np
+from collections import Counter
+
+settings = [
+    {
+        'p': 0.04,
+        'fn': 'typo.drop_n_next_twice',
+        'args': {'loc': ['m', 'e'], 'keep_case': True}
+    },
+    {
+        'p': 0.04,
+        'fn': 'typo.swap_consecutive',
+        'args': {'loc': ['m', 'e'], 'keep_case': True}
+    },
+    {
+        'p': 0.02,
+        'fn': 'typo.pressed_twice',
+        'args': {'loc': 'u', 'keep_case': True}
+    },
+    {
+        'p': 0.02,
+        'fn': 'typo.drop_char',
+        'args': {'loc': ['m', 'e'], 'keep_case': True}
+    },
+    {
+        'p': 0.02,
+        'fn': 'typo.pressed_shiftalt',
+        'args': {'loc': ['b', 'm']},
+        'keymap': kbl.macbook_us,
+        'trans': kbl.keyboard_transprob
+    },
+]
+
+def test1():
+    np.random.seed(seed=42)
+    word = "Blume"
+    newwords = []
+    for i in range(100):
+        newwords.append( wordaug(word, settings) )
+    assert list(dict(Counter(newwords)).keys()) == [
+        'Blume', 'Blum', 'Bllume', 'Bmme', 'Blumee', 'Bblum',
+        'BlUme', 'Bluee', 'Bluem', 'Buume', 'Blmme']
